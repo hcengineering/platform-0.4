cf6d420f3d6add0bb109d6ab5f6486947d055cd7
Andrey Sobolev <haiodo@gmail.com>
2021-06-14 17:29:35 +0700

    Do not use esModuleInterop
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- packages/core/src/__tests__/model.tx.ts
+++ packages/core/src/__tests__/model.tx.ts
@@ -1,6 +1,6 @@
 import { Tx } from '../tx'
 
-import txImp from './model.tx.json'
+const txImp = require('./model.tx.json') // eslint-disable-line @typescript-eslint/no-var-requires
 const txs = txImp as unknown as Tx[]
 
 export { txs as default }
