c64401f1183177d8973b0000f33d4e48e3e4579c
Denis Bykhov <bykhov.denis@gmail.com>
2021-06-17 12:39:48 +0600

    implemented `$in` query for memdb
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- packages/core/src/memdb.ts
+++ packages/core/src/memdb.ts
@@ -17,14 +17,18 @@ import { PlatformError, Severity, Status } from '@anticrm/status'
 import type { Class, Collection, Doc, Emb, PrimitiveType, Ref } from './classes'
 import core from './component'
 import type { Hierarchy } from './hierarchy'
-import { DocumentQuery, Storage } from './storage'
+import { DocumentQuery, QuerySelector, Storage } from './storage'
 import { Tx, TxAddCollection, TxCreateDoc, TxProcessor } from './tx'
 import { generateId, makeEmb } from './utils'
 
-function findProperty (objects: Doc[], propertyKey: string, value: PrimitiveType): Doc[] {
+function findProperty<T extends Doc> (objects: Doc[], propertyKey: string, value: PrimitiveType | QuerySelector<T>): Doc[] {
   const result: Doc[] = []
   for (const object of objects) {
-    if ((object as any)[propertyKey] === value) {
+    if (typeof value === 'object') {
+      if ((value.$in?.includes((object as any)[propertyKey])) === true) {
+        result.push(object)
+      }
+    } else if ((object as any)[propertyKey] === value) {
       result.push(object)
     }
   }
@@ -87,7 +91,7 @@ class MemDb {
     for (const key in query) {
       if (key === '_id') continue
       const value = (query as any)[key]
-      result = findProperty(result, key, value)
+      result = findProperty<T>(result, key, value)
     }
     return [...result] as T[]
   }
