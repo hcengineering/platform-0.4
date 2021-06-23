83316a786f6c70e29be2871678ccc1709301892e
Andrey Sobolev <haiodo@gmail.com>
2021-06-23 15:05:02 +0700

    Fix #127
    Fix #127
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- packages/text/src/marks.ts
+++ packages/text/src/marks.ts
@@ -1,4 +1,5 @@
 import { MessageMark, MessageMarkType, MessageNode } from './model'
+import { deepEqual } from 'fast-equals'
 
 export function traverseMarks (node: MessageNode, f: (el: MessageMark) => void): void {
   node.marks?.forEach(f)
@@ -27,9 +28,8 @@ export function removeFromSet (markType: MessageMarkType, marks: MessageMark[]):
 
 export function sameSet (a: MessageMark[] | undefined, b: MessageMark[] | undefined): boolean {
   if (a === b) return true
-  if (a === undefined) return false
-  if (b === undefined) return false
-  if (a.length !== b.length) return false
+  if (a === undefined || b === undefined || a.length !== b.length) return false
+
   for (let i = 0; i < a.length; i++) {
     if (!markEq(a[i], b[i])) {
       return false
@@ -39,22 +39,5 @@ export function sameSet (a: MessageMark[] | undefined, b: MessageMark[] | undefi
 }
 
 export function markEq (first: MessageMark, other: MessageMark): boolean {
-  return first === other || (first.type === other.type && compareDeep(first.attrs, other.attrs))
-}
-
-function compareDeep (a: any, b: any): boolean {
-  if (a === b) return true
-  if (!(a !== undefined && typeof a === 'object') || !(b !== undefined && typeof b === 'object')) return false
-  const isarray = Array.isArray(a)
-  if (Array.isArray(b) !== isarray) return false
-  if (isarray) {
-    if (a.length !== b.length) return false
-    for (let i = 0; i < a.length; i++) {
-      if (!compareDeep(a[i], b[i])) return false
-    }
-  } else {
-    for (const p in a) if (!(p in b) || !compareDeep(a[p], b[p])) return false
-    for (const p in b) if (!(p in a)) return false
-  }
-  return true
+  return first === other || (first.type === other.type && deepEqual(first.attrs, other.attrs))
 }
