29f18d1965247a63ba998adea2848315ee10eac2
Andrey Platov <andrey.v.platov@gmail.com>
2021-06-25 00:45:28 +0200

    fix `plugin-core-dev` tests
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- plugins/plugin-core-dev/src/index.ts
+++ plugins/plugin-core-dev/src/index.ts
@@ -1,14 +1,14 @@
 //
 // Copyright © 2020 Anticrm Platform Contributors.
-// 
+//
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
 // obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
@@ -26,10 +26,9 @@ import { connect } from './connection'
  * Licensed under the Eclipse Public License, Version 2.0
  */
 export default async (): Promise<CoreService> => {
+  let client: (Client & TxOperations) | undefined
 
-  let client: Client & TxOperations | undefined
-
-  async function getClient(): Promise<Client & TxOperations> {
+  async function getClient (): Promise<Client & TxOperations> {
     if (client === undefined) {
       const storage = await createClient(connect)
       client = withOperations(core.account.System, new LiveQuery(storage))
