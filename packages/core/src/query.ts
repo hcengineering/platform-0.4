2cc86e289480d66fc3f65d1b41f8c13dd46e34d7
Denis Bykhov <bykhov.denis@gmail.com>
2021-07-06 17:14:57 +0600

    fix
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- packages/core/src/query.ts
+++ packages/core/src/query.ts
@@ -30,7 +30,7 @@ export function findProperty (objects: Doc[], propertyKey: string, value: any): 
 export function resultSort<T extends Doc> (result: T[], sortOptions: SortingQuery<T>): void {
   const sortFunc = (a: any, b: any): number => {
     for (const key in sortOptions) {
-      const result = typeof a[key] === 'string' ? a[key].localeCompare(b[key]) : a - b
+      const result = typeof a[key] === 'string' ? a[key].localeCompare(b[key]) : a[key] - b[key]
       if (result !== 0) return result * (sortOptions[key] as number)
     }
     return 0
