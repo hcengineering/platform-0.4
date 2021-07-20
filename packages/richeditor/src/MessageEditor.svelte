14783892b0950141361adc8decdafda3cb5d0a27
Alexander Platov <sas_lord@mail.ru>
2021-07-20 15:02:52 +0300

    Fix placeholder in ReferenceInput and MDRefEditor (#230)
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- packages/richeditor/src/MessageEditor.svelte
+++ packages/richeditor/src/MessageEditor.svelte
@@ -364,7 +364,8 @@ export function focus (): void {
       outline: none;
 
       p {
-        margin: 5px;
+        margin: 0px;
+        color: var(--theme-caption-color);
       }
     }
   }
@@ -375,11 +376,13 @@ export function focus (): void {
 }
 
 .hover-message {
-  position: relative;
-  top: 0px;
+  // position: relative;
+  // top: 0px;
   pointer-events: none;
-  padding-left: 5px;
-  padding-top: 5px;
-  color: #aaaaaa;
+  user-select: none;
+  // padding-left: 5px;
+  // padding-top: 5px;
+  color: var(--theme-content-trans-color);
+  // background-color: red;
 }
 </style>
