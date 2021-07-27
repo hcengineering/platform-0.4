eee9e805e3c42479812d929ea15e558218c991df
Alexander Platov <sas_lord@mail.ru>
2021-07-27 19:04:35 +0700

    add in index.ts
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -48,6 +48,7 @@ export { default as CompletionPopup } from './components/CompletionPopup.svelte'
 export { default as Table } from './components/Table.svelte'
 export { default as Kanban } from './components/kanban/Kanban.svelte'
 export { default as DatePicker } from './components/DatePicker.svelte'
+export { default as Section } from './components/Section.svelte'
 
 export { default as IconAdd } from './components/icons/Add.svelte'
 export { default as IconClose } from './components/icons/Close.svelte'
