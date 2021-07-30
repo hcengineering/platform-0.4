08f72097f1c4c939b1b81bd17ae644b1edad3bb4
Alexander Platov <sas_lord@mail.ru>
2021-07-30 17:10:56 +0300

    Conflict
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- plugins/task-impl/src/components/CreateProject.svelte
+++ plugins/task-impl/src/components/CreateProject.svelte
@@ -13,15 +13,34 @@
 // limitations under the License.
 -->
 <script lang="ts">
-  import { EditBox, Dialog, TextArea, ToggleWithLabel, Section, IconFile, IconComments, Grid } from '@anticrm/ui'
+  import {
+    EditBox,
+    Dialog,
+    TextArea,
+    ToggleWithLabel,
+    Section,
+    IconFile,
+    IconComments,
+    UserBox,
+    Grid
+  } from '@anticrm/ui'
   import { getClient } from '@anticrm/workbench'
 
   import task from '../plugin'
   import core from '@anticrm/core'
+  import type { Account, Ref } from '@anticrm/core'
 
   let name: string = ''
   let description: string = ''
   let isPrivate: boolean = false
+  let members: Ref<Account>[] = []
+  let users: Account[] = []
+  let newMember: Ref<Account> | undefined
+  $: if (newMember !== undefined) {
+    members.push(newMember)
+    members = members
+    newMember = undefined
+  }
 
   const client = getClient()
 
@@ -30,13 +49,27 @@
       name,
       description,
       private: isPrivate,
-      members: [client.accountId()]
+      members: members
     })
+  }
+
+  function filterMembers (): void {
+    members = members.filter((m) => m !== undefined)
+  }
+
+  function getAvaibleMembers (members: Ref<Account>[], member?: Ref<Account>): Account[] {
+    return member === undefined
+      ? users.filter((p) => !members.includes(p._id))
+      : users.filter((p) => !members.includes(p._id) || member === p._id)
+  }
+
+  async function getUsers (): Promise<void> {
+    users = await client.findAll(core.class.Account, {})
   }
 </script>
 
 <Dialog label={task.string.CreateProject} okLabel={task.string.CreateProject} okAction={createProject} on:close>
-  <Section icon={IconFile} label={'General Information'}>
+  <Section label={task.string.GeneralInformation} icon={IconFile}>
     <Grid column={1}>
       <EditBox label={task.string.ProjectName} bind:value={name} />
       <TextArea label={task.string.ProjectDescription} bind:value={description} />
@@ -47,5 +80,44 @@
       />
     </Grid>
   </Section>
-  <Section icon={IconComments} label={'Project Members'} closed />
+  <Section label={task.string.ProjectMembers} icon={IconComments} topLine>
+    {#await getUsers() then users}
+      <Grid column={1}>
+        {#each members as member}
+          <UserBox
+            bind:selected={member}
+            users={getAvaibleMembers(members, member)}
+            title={task.string.IviteMember}
+            label={task.string.IviteMember}
+            showSearch
+            on:change={() => {
+              filterMembers()
+            }}
+          />
+        {/each}
+        {#if getAvaibleMembers(members).length}
+          <UserBox
+            bind:selected={newMember}
+            users={getAvaibleMembers(members)}
+            title={task.string.IviteMember}
+            label={task.string.IviteMember}
+            showSearch
+          />
+        {/if}
+      </Grid>
+    {/await}
+  </Section>
 </Dialog>
+
+<style lang="scss">
+  .content {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 20px;
+
+    .row {
+      grid-column-start: 1;
+      grid-column-end: 3;
+    }
+  }
+</style>
