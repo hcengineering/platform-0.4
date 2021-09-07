2fa844182a70754ff48f1ebf8f9462d628d43ee5
Andrey Sobolev <haiodo@gmail.com>
2021-09-07 15:18:02 +0700

    Allow gh redirect
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- dev/prod/build_demo.sh
+++ dev/prod/build_demo.sh
@@ -3,6 +3,7 @@ rev=$(git rev-parse HEAD)
 version="0.4.0-${rev}"
 
 cp ./public/favicon.png ./dist/
+
 cat << EOF > ./dist/index.html
 <!doctype html>
 <html>
@@ -15,7 +16,17 @@ cat << EOF > ./dist/index.html
 
 	<link rel='icon' type='image/png' href='${PREFIX}/favicon.png'>
 	<link rel='stylesheet' href='/platform/bundle.css'>
+
+    <script type="text/javascript">
+		const key = 'Platform_404_Uri'
+		var loc = localStorage.getItem(key)
+		if( loc ) {
+			localStorage.removeItem(key)
+			window.history.replaceState(null, null, loc)
+		}
+    </script>
 </head>
+
 
 <body style="margin: 0; overflow: hidden;">
 	<script>
@@ -24,5 +35,25 @@ cat << EOF > ./dist/index.html
 	<script src='${PREFIX}/bundle.js'></script>
 </body>
 
+</html>
+EOF
+
+cat << EOF > ./dist/404.html
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Anticrm Platform 404 redirect</title>
+    <script type="text/javascript">      
+		var l = window.location;
+
+		const key = 'Platform_404_Uri'
+		var loc = localStorage.setItem(key, window.location)
+      	
+		l.replace( l.protocol + '//' + l.hostname + '/platform/' )
+    </script>
+  </head>
+  <body>
+  </body>
 </html>
 EOF
